fcb4b39dd69ce9876eb01ad9ee88f8dbbb8de77e
artursm14 <antonioartur123456@gmail.com>
2024-09-18 21:56:27 -0300

    changes repository and controller and routes

--- backend-alert-panel/repositories/roomRepository.js
+++ backend-alert-panel/repositories/roomRepository.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { ROOMS_COLLECTION } from "../utils/constants/collections.js";
 
 const RoomRepository = {
@@ -8,15 +9,41 @@ const RoomRepository = {
         return await db.collection(ROOMS_COLLECTION).find().toArray();
     },
     async getRoomById(db, id) {
-        return await db.collection(ROOMS_COLLECTION).findOne({ _id: id });
+        const idAsObejectId =  ObjectId.createFromHexString(id);
+
+        return await db.collection(ROOMS_COLLECTION).findOne({ _id: idAsObejectId });
     },
     async updateRoom(db, id, room) {
-        await db.collection(ROOMS_COLLECTION).updateOne({ _id: id }, { $set: room });
+        const isAsObjectId =  ObjectId.createFromHexString(id);
+
+        await db.collection(ROOMS_COLLECTION).updateOne({ _id: isAsObjectId }, { $set: room });
     },
     async deleteRoom(db, id) {
-        await db.collection(ROOMS_COLLECTION).deleteOne({ _id: id });
-    }
+        const isAsObjectId =  ObjectId.createFromHexString(id);
+        await db.collection(ROOMS_COLLECTION).deleteOne({ _id: isAsObjectId });
+    },
+    async repairRoom(db, id) {
+        const isAsObjectId =  ObjectId.createFromHexString(id);
 
+        await db.collection(ROOMS_COLLECTION).updateOne({ _id: isAsObjectId },
+            { $set: { redirect: null, inoperatedResources: [] } });
+    },
+    async redirectRoom(db, id, redirect) {
+        const idAsObejectId =  ObjectId.createFromHexString(id);
+        const rediretAsObjectId =  ObjectId.createFromHexString(redirect.to);
+
+        await db.collection(ROOMS_COLLECTION).updateOne({ _id: idAsObejectId},
+            { $set: { redirect: {
+                to : rediretAsObjectId,
+                experationDate: redirect.experationDate
+            } } });
+    },
+    async submitIssueRoom(db, id, inoperatedResources) {
+        const idAsObejectId =  ObjectId.createFromHexString(id);
+        
+        await db.collection(ROOMS_COLLECTION).updateOne({ _id: idAsObejectId },
+            { $set: { inoperatedResources } });
+    },
 }
 
 export default RoomRepository;
